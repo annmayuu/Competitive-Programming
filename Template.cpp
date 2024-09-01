08c4bb8dde1b03d80259adc56afa58cf1a76e3fd
May <annmayuu@gmail.com>
2024-09-01 13:29:53 -0600

    Add files via upload
    Template for c++

---
+++ Template.cpp
@@ -0,0 +1,24 @@
+/*Template for c++ made by May (annmayuu)*/
+#include <bits/stdc++.h>
+#define endl '\n'
+#define forn(i,n) for(int i=0;i<int(n);i++)
+#define forr(i,a,b) for (int i = int(a); i <= int(b); i++)
+#define fore(obj, element) for(const auto& element: obj)
+#define pb push_back
+typedef long long lli;
+using namespace std;
+ 
+void solution(){
+    //Put your solution here
+}
+ 
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int T=1;
+    cin>>T;
+ 
+	while(T--){
+		solution();
+	}
+	return 0;
+}
